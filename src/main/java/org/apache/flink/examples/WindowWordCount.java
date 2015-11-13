5d5c90d9238e312959a36ab1c092087ddba77151
smarthi <smarthi@apache.org>
2015-11-12 19:07:01 -0500

    Streaming Window Word Count Example

---
+++ src/main/java/org/apache/flink/examples/WindowWordCount.java
@@ -0,0 +1,47 @@
+package org.apache.flink.examples;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Streaming Window Word Count Example
+ */
+public class WindowWordCount {
+
+  public static void main(String[] args) throws Exception {
+    // Get an instance of the Streaming Execution Environment
+    final StreamExecutionEnvironment env =
+        StreamExecutionEnvironment.getExecutionEnvironment();
+
+    // Create a DataStream from the streaming input
+    DataStream<String> lines =
+        env.socketTextStream("localhost", 9999);
+
+    DataStream<Tuple2<String, Integer>> counts =
+        lines.flatMap(new LineSplitter())
+            .keyBy(0)
+            // counts for words every 5 seconds
+            .timeWindow(Time.of(5, TimeUnit.SECONDS))
+            .sum(1);
+
+    counts.print();
+
+    // Process the DataStream
+    env.execute("Streaming Word Count");
+  }
+
+  public static class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+    @Override
+    public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+      for (String word : s.split(" ")) {
+        collector.collect(new Tuple2<>(word, 1));
+      }
+    }
+  }
+}
