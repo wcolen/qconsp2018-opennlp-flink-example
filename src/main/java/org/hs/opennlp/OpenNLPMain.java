a642c93b0c4847d77e36ff8af12e32524d3019e8
Jörn Kottmann <joern@apache.org>
2017-06-15 10:24:50 +0200

    Some changes

--- src/main/java/org/hs/opennlp/OpenNLPMain.java
+++ src/main/java/org/hs/opennlp/OpenNLPMain.java
@@ -1,15 +1,10 @@
 package org.hs.opennlp;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.io.TextInputFormat;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -17,13 +12,11 @@ import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.FileProcessingMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import opennlp.tools.chunker.ChunkerME;
 import opennlp.tools.chunker.ChunkerModel;
-import opennlp.tools.langdetect.Language;
 import opennlp.tools.langdetect.LanguageDetectorME;
 import opennlp.tools.langdetect.LanguageDetectorModel;
 import opennlp.tools.postag.POSModel;
@@ -33,9 +26,6 @@ import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
 
-/**
- * Created by smarthi on 6/2/17.
- */
 public class OpenNLPMain {
 
   private static final Logger LOG = LoggerFactory.getLogger(OpenNLPMain.class);
@@ -62,52 +52,58 @@ public class OpenNLPMain {
     streamExecutionEnvironment.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
 
     DataStream<String> inputStream =
-        streamExecutionEnvironment.readTextFile(OpenNLPMain.class.getResource("/input.txt").getFile());
+        streamExecutionEnvironment.readTextFile(
+        OpenNLPMain.class.getResource("/input/eng_news_2015_100K-sentences.txt").getFile());
 
-    DataStream<String> textStream = inputStream.filter(new MyFilter());
+    // read german text
 
-    SplitStream<String> langStream = textStream.split(new LanguageSelector());
+    // read english text
 
-    DataStream<String> engLangStream = langStream.select("eng");
-//    DataStream<String> germanStream = langStream.select("deu");
+    DataStream<String> newsArticles = inputStream.map(new LeipzigParser());
 
-    DataStream<String[]> sentences = engLangStream.map(new SentenceMapFunction());
+    SplitStream<String> langStream = newsArticles.split(new LanguageSelector());
 
-    sentences.writeAsText("/Users/smarthi/bbuzz/src/main/resources/sentences.txt", FileSystem.WriteMode.OVERWRITE);
+    DataStream<String> engNewsArticles = langStream.select("eng");
+
+    DataStream<String> deuNewsArticles = langStream.select("deu");
+
+    DataStream<String> itaNewsArticles = langStream.select("ita");
 
 
-    textStream.writeAsText("/Users/smarthi/bbuzz/src/main/resources/output.txt", FileSystem.WriteMode.OVERWRITE);
+
+//     DataStream<String[]> sentences = engLangStream.map(new SentenceMapFunction());
+
+
+   // sentences.writeAsText("/Users/smarthi/bbuzz/src/main/resources/sentences.txt", FileSystem.WriteMode.OVERWRITE);
+
+
+    engNewsArticles.writeAsText("output.txt", FileSystem.WriteMode.OVERWRITE);
 
     streamExecutionEnvironment.execute();
 
   }
 
   private void initializeModels() throws IOException {
-    try (InputStream sentenceDetectorModel = OpenNLPMain.class.getResourceAsStream("/opennlp-models/en-sent.bin")) {
-      sentenceDetector = new SentenceDetectorME(new SentenceModel(sentenceDetectorModel));
-    }
+    sentenceDetector = new SentenceDetectorME(new SentenceModel(
+        OpenNLPMain.class.getResource("/opennlp-models/en-sent.bin")));
 
-    try (InputStream tokenizerModel = OpenNLPMain.class.getResourceAsStream("/opennlp-models/en-token.bin")) {
-      tokenizer = new TokenizerME(new TokenizerModel(tokenizerModel));
-    }
+    tokenizer = new TokenizerME(new TokenizerModel(
+        OpenNLPMain.class.getResource("/opennlp-models/en-token.bin")));
 
-    try (InputStream posModel = OpenNLPMain.class.getResourceAsStream("/opennlp-models/en-pos-maxent.bin")) {
-      posTagger = new POSTaggerME(new POSModel(posModel));
-    }
+    posTagger = new POSTaggerME(new POSModel(
+        OpenNLPMain.class.getResource("/opennlp-models/en-pos-maxent.bin")));
 
-    try (InputStream chunkerModel = OpenNLPMain.class.getResourceAsStream("/opennlp-models/en-chunker.bin")) {
-      chunker = new ChunkerME(new ChunkerModel(chunkerModel));
-    }
+    chunker = new ChunkerME(new ChunkerModel(
+        OpenNLPMain.class.getResource("/opennlp-models/en-chunker.bin")));
 
-    try (InputStream languageModel = OpenNLPMain.class.getResourceAsStream("/opennlp-models/lang-maxent.bin")) {
-      languageDetectorME = new LanguageDetectorME(new LanguageDetectorModel(languageModel));
-    }
+    languageDetectorME = new LanguageDetectorME(new LanguageDetectorModel(
+        OpenNLPMain.class.getResource("/opennlp-models/lang-maxent.bin")));
   }
 
-  private static class MyFilter implements FilterFunction<String> {
+  private static class LeipzigParser implements MapFunction<String, String> {
     @Override
-    public boolean filter(String s) throws Exception {
-      return s.trim().length() > 0 && !(s.startsWith("=") || !Character.isAlphabetic(s.charAt(0)));
+    public String map(String s) throws Exception {
+      return s.substring(s.indexOf('\t') + 1);
     }
   }
 
@@ -128,5 +124,4 @@ public class OpenNLPMain {
       return sentenceDetector.sentDetect(s);
     }
   }
-
 }
