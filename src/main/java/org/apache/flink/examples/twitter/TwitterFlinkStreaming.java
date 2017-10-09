fa0a9e050af19106f27792694e6c84c3a995a6eb
smarthi <smarthi@apache.org>
2017-10-09 15:06:56 -0400

    MlConf talk prep
    pom change
    
    rewrote twitter flink streaming example

--- src/main/java/org/apache/flink/examples/twitter/TwitterFlinkStreaming.java
+++ src/main/java/org/apache/flink/examples/twitter/TwitterFlinkStreaming.java
@@ -1,16 +1,22 @@
 package org.apache.flink.examples.twitter;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.endpoint.StreamingEndpoint;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.shaded.com.google.common.collect.Lists;
 import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.Serializable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * Main Twitter Streaming Demo class
@@ -25,25 +31,28 @@ public class TwitterFlinkStreaming {
     // Get an instance of the Streaming Execution Environment
     final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment().setParallelism(1);
 
+    // twitter credentials & source
+    Properties props = new Properties();
+    props.load(TwitterFlinkStreaming.class.getResourceAsStream("/twitter.properties"));
+    TwitterSource twitterSource = new TwitterSource(props);
+    twitterSource.setCustomEndpointInitializer(new MyFilterEndpoint());
+
     // create a DataStream from TwitterSource
-    DataStream<Tweet> twitterStream =
-        env.addSource(new TwitterSource(
-            TwitterFlinkStreaming.class.getResource("/twitter.properties").getFile(),
-            new String[]{"#MondayMotivation", "#ColumbusDay"}));
+    DataStream<String> twitterStream = env.addSource(twitterSource);
 
     // Split the Stream based on the Selector criterion - '#DCFlinkMeetup' and others
-    SplitStream<Tweet> tweetSplitStream = twitterStream.split(new SplitSelector());
+    SplitStream<String> tweetSplitStream = twitterStream.split(new SplitSelector());
 
-    DataStream<Tweet> dcFlinkTweetStream = tweetSplitStream.select("apacheBigData");
+    DataStream<String> dcFlinkTweetStream = tweetSplitStream.select("apacheBigData");
 
-    DataStream<Tweet> otherTweetStream = tweetSplitStream.select("ColumbusDay");
+    DataStream<String> otherTweetStream = tweetSplitStream.select("ColumbusDay");
 
     // Persist the Split streams as Text to local filesystem, overwrites any previous files that may exist
     dcFlinkTweetStream.writeAsText("/tmp/MondayMotivation", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
     otherTweetStream.writeAsText("/tmp/ColumbusDay", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
 
     // Join two different streams
-//    JoinedStreams<Tweet, Tweet> tweetStream = dcFlinkTweetStream.join(otherTweetStream);
+    //    JoinedStreams<Tweet, Tweet> tweetStream = dcFlinkTweetStream.join(otherTweetStream);
 
     // TODO: Add Tumbling Window Semantics
 
@@ -54,14 +63,13 @@ public class TwitterFlinkStreaming {
   /**
    * Split the input {@link DataStream} into two DataStreams based on the select criterion
    */
-  public static class SplitSelector implements OutputSelector<Tweet> {
-
+  public static class SplitSelector implements OutputSelector<String> {
     @Override
-    public Iterable<String> select(Tweet tweet) {
+    public Iterable<String> select(String tweet) {
       LOG.info(tweet.toString());
 
       List<String> list = new ArrayList<>();
-      if (tweet.getText().toLowerCase().contains("#dcflinkmeetup")) {
+      if (tweet.toLowerCase().contains("#dcflinkmeetup")) {
         list.add("DCFlinkMeetup");
       } else {
         list.add("Others");
@@ -70,10 +78,20 @@ public class TwitterFlinkStreaming {
     }
   }
 
-  private static class MyFilterFunction implements FilterFunction<Tweet> {
+  private static class MyFilterFunction implements FilterFunction<String> {
     @Override
-    public boolean filter(Tweet tweet) throws Exception {
+    public boolean filter(String tweet) throws Exception {
       return false;
+    }
+  }
+
+  private static class MyFilterEndpoint implements TwitterSource.EndpointInitializer, Serializable {
+
+    @Override
+    public StreamingEndpoint createEndpoint() {
+      StatusesFilterEndpoint ep = new StatusesFilterEndpoint();
+      ep.trackTerms(Lists.newArrayList("MondayMotivation", "ColumbusDay"));
+      return ep;
     }
   }
 }
