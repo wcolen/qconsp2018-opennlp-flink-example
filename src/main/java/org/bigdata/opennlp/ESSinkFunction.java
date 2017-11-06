c3bb876c8d57fbafec38dafc0c3ca3c5196d0a28
Jörn Kottmann <joern@apache.org>
2017-11-06 23:34:32 +0100

    Index a few fields into ES

--- src/main/java/org/bigdata/opennlp/ESSinkFunction.java
+++ src/main/java/org/bigdata/opennlp/ESSinkFunction.java
@@ -1,41 +1,31 @@
 package org.bigdata.opennlp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.flink.api.common.functions.RuntimeContext;
+import org.apache.flink.examples.news.NewsArticle;
 import org.apache.flink.streaming.connectors.elasticsearch.ElasticsearchSinkFunction;
 import org.apache.flink.streaming.connectors.elasticsearch.RequestIndexer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Requests;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class ESSinkFunction<T> implements ElasticsearchSinkFunction<Annotation<T>> {
-
-  private final String index;
-  private final String type;
-
-  public ESSinkFunction(final String index, final String type) {
-    this.index = index;
-    this.type = type;
-  }
+public class ESSinkFunction implements ElasticsearchSinkFunction<Annotation<NewsArticle>> {
 
   @Override
-  public void process(Annotation<T> annotation, RuntimeContext runtimeContext, RequestIndexer requestIndexer) {
-    requestIndexer.add(createIndexRequest(annotation));
+  public void process(Annotation<NewsArticle> element, RuntimeContext ctx, RequestIndexer indexer) {
+
+	Map<String, String> json = new HashMap<>();
+	json.put("id", element.getId());
+	json.put("text", element.getSofa());
+	json.put("headline", element.getSofa().substring(
+		element.getHeadline().getStart(), element.getHeadline().getEnd()));
+
+	IndexRequest request = Requests.indexRequest()
+		.index("my-index")
+		.type("news-article")
+		.source(json);
+
+	indexer.add(request);
   }
-
-  public IndexRequest createIndexRequest(Annotation<T> annotation) {
-    Map<String, String> mapping = new HashMap<>();
-    mapping.put("id", annotation.getId());
-    mapping.put("headline", annotation.getSofa().substring(annotation.getHeadline().getStart(), annotation.getHeadline().getEnd()));
-    /*
-    map more fields
-     */
-
-    return Requests.indexRequest()
-            .index(index)
-            .type(type)
-            .source(mapping);
-  }
-
 }
--- src/main/java/org/bigdata/opennlp/NewsPipeline.java
+++ src/main/java/org/bigdata/opennlp/NewsPipeline.java
@@ -1,16 +1,23 @@
 package org.bigdata.opennlp;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.examples.news.AnnotationInputFormat;
 import org.apache.flink.examples.news.NewsArticle;
 import org.apache.flink.examples.news.NewsArticleAnnotationFactory;
+import org.apache.flink.hadoop.shaded.com.google.common.collect.Lists;
 import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.elasticsearch5.ElasticsearchSink;
 
 import opennlp.tools.namefind.TokenNameFinderModel;
 import opennlp.tools.postag.POSModel;
@@ -57,17 +64,16 @@ public class NewsPipeline {
         .map(new POSTaggerFunction<>(engPosModel)).setParallelism(2)
         .map(new NameFinderFunction<>(engNerPersonModel));
 
-/*
-    Map<String,String> config = Maps.newHashMap();
-    config.put("cluster.name", "my-cluster-name");
+    Map<String,String> config = new HashMap<>();
+    config.put("cluster.name", "docker-cluster");
     config.put("bulk.flush.max.actions", "1000");
 
     List<InetSocketAddress> transportAddresses = Lists.newArrayList(
             new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 9300),
             new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 9301));
 
-    analyzedEng.addSink(new ElasticsearchSink<>(config, transportAddresses, new ESSinkFunction<>("bigdata","article")));
-*/
+    analyzedEng.addSink(new ElasticsearchSink<Annotation<NewsArticle>>(config, transportAddresses, new ESSinkFunction()));
+
 
 /*
     SingleOutputStreamOperator eng2 = articleStream.flatMap((FlatMapFunction<Annotation<NewsArticle>, Tuple2<String, Integer>>)
