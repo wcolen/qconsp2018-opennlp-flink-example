55c35825edc77abc5fc3259a7c582a34196543f5
Peter Thygesen <thygesen@apache.org>
2017-11-12 23:21:55 +0100

    added date

--- src/main/java/org/bigdata/opennlp/ESSinkFunction.java
+++ src/main/java/org/bigdata/opennlp/ESSinkFunction.java
@@ -1,11 +1,6 @@
 package org.bigdata.opennlp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import opennlp.tools.tokenize.SimpleTokenizer;
 import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.examples.news.NewsArticle;
 import org.apache.flink.streaming.connectors.elasticsearch.ElasticsearchSinkFunction;
@@ -13,23 +8,30 @@ import org.apache.flink.streaming.connectors.elasticsearch.RequestIndexer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Requests;
 
-import opennlp.tools.tokenize.SimpleTokenizer;
-import opennlp.tools.util.Span;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ESSinkFunction implements ElasticsearchSinkFunction<Annotation<NewsArticle>> {
 
   private String entityKey(CharSequence mention) {
 
     String[] mentionTokens = SimpleTokenizer.INSTANCE.tokenize(mention.toString());
-	Arrays.sort(mentionTokens);
+    // join and clean non alphanum
+    return Arrays.stream(mentionTokens)
+            .map(t -> t.toLowerCase()).sorted()
+            .collect(Collectors.joining(" "))
+            .replaceAll("[^\\p{L}\\p{Nd}]+", "");
+		/*
+		Arrays.sort(mentionTokens);
 
-	StringBuilder key = new StringBuilder();
+		StringBuilder key = new StringBuilder();
 
-	for (String token : mentionTokens) {
-	  key.append(token.toLowerCase());
-	}
+		for (String token : mentionTokens) {
+			key.append(token.toLowerCase());
+		}
 
-	return key.toString();
+		return key.toString();
+		*/
   }
 
   @Override
@@ -38,6 +40,9 @@ public class ESSinkFunction implements ElasticsearchSinkFunction<Annotation<News
 	Map<String, Object> json = new HashMap<>();
 	json.put("id", element.getId());
 	json.put("text", element.getSofa());
+	json.put("lang", element.getLanguage());
+	json.put("date", element.getPiggyback().getPublicationDate());
+	json.put("source", element.getPiggyback().getSourceName());
 	json.put("headline", element.getSofa().substring(
 		element.getHeadline().getStart(), element.getHeadline().getEnd()));
 
@@ -51,7 +56,7 @@ public class ESSinkFunction implements ElasticsearchSinkFunction<Annotation<News
 	  }
 	}
 
-	json.put("entity-keys", entityKeys);
+	json.put("entity-keys", entityKeys.toArray(new String[entityKeys.size()]));
 
 	IndexRequest request = Requests.indexRequest()
 		.index("my-index")
